f548a3dd2407668ffc0cc4f523c8a7dd065bf103
王焱辉 <wyh_19@163.com>
2019-01-05 21:23:32 +0800

    修改mongo端口

--- action/8/photo_app/models/Photo.js
+++ action/8/photo_app/models/Photo.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/photo_app');
+mongoose.connect('mongodb://127.0.0.1:27017/photo_app');
 
 var schema = new mongoose.Schema({
   name: String,
